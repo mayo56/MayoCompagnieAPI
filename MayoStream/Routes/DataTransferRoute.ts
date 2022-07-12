69db5b3947f2d01612a91192e3860b634df97844
Mayo <66140252+mayo56@users.noreply.github.com>
2022-07-13 00:02:35 +0200

    Update DataTransferRoute.ts

--- MayoStream/Routes/DataTransferRoute.ts
+++ MayoStream/Routes/DataTransferRoute.ts
@@ -1,8 +1,21 @@
 import express from "express";
+import multer from "multer";
 const DataTransferR = express();
 
 import DataTransferC from "../Controller/DataTransferController";
 
-DataTransferR.get("/:id", DataTransferC.getVideo)
+const storageVideo = multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, 'MayoStream/Videos');
+    },
+    filename: function (req, file, cb) {
+        const name = Date.now().toString() + `.${file.mimetype.slice(6)}`;
+        cb(null, "video" + '-' + name);
+    }
+});
+const videoStock = multer({ storage: storageVideo });
+
+DataTransferR.get("/:id", DataTransferC.getVideo);
+DataTransferR.post("/post", videoStock.single("file"), DataTransferC.postVideo);
 
 export default DataTransferR;
