36266e88d9b0e806f8345cd7bb442240fd3f6e7a
Mayo <66140252+mayo56@users.noreply.github.com>
2022-07-13 00:01:54 +0200

    Update DataTransferController.ts

--- MayoStream/Controller/DataTransferController.ts
+++ MayoStream/Controller/DataTransferController.ts
@@ -1,9 +1,14 @@
 import express from "express";
 
 const DataTransferC = {
-    getVideo:async (req:express.Request, res:express.Response) => {
-        res.send("Lolilol")
-    }
+    getVideo: async (req:express.Request, res:express.Response) => {
+        
+    },
+    postVideo: async (req:express.Request, res:express.Response) => {
+        res.status(201).send("Ok !")
+        console.log(req)
+    },
+
 }
 
 export default DataTransferC;
