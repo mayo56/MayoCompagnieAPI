67f4fe8430b501a17bf9e59a66abbec746f2e015
mayo56 <maxinou56.lr@gmail.com>
2022-07-15 22:02:57 +0200

    dsd

--- MayoStream/Controller/DataTransferController.ts
+++ MayoStream/Controller/DataTransferController.ts
@@ -2,7 +2,7 @@ import express from "express";
 import path from "node:path";
 
 const DataTransferC = {
-    getVideo: async (req: express.Request, res: express.Response, next:express.NextFunction) => {
+    getVideo: async (req: express.Request, res: express.Response, next: express.NextFunction) => {
         let OptionGetVideo = {
             root: path.join(__dirname, '../Videos'),
             dotfiles: 'deny',
@@ -14,12 +14,19 @@ const DataTransferC = {
         res.sendFile((`video-${req.params.id}`), OptionGetVideo, (err) => {
             if (err) {
                 next(err);
-            };
-        });
+            }
+        })
+    },
+    getInfoVideo: async (req: express.Request, res: express.Response, next: express.NextFunction) => {
+        const data = {
+            //some data here (as reponse from db)
+        };
+        if (data) {return res.status(201).send(data)}
+        else {return res.status(401).send({error : "video not found"})}
     },
     postVideo: async (req: express.Request, res: express.Response) => {
-        if (!req.file) return res.status(401).send({error: "Pas de vidéo envoyé."})
-        else return res.status(201).send("Ok !");
+        if (!req.file) {return res.status(401).send({ error: "Don't have video" })}
+        else {return res.status(201).send("Ok !");}
     },
 
 }
