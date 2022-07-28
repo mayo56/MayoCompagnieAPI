5b11ced8bc8efef7ddf7704c277c695467eaf1f4
mayo56 <maxinou56.lr@gmail.com>
2022-07-28 14:49:14 +0200

    Première maj MayoGenda

--- MayoGenda/Controllers/DataControllerGenda.ts
+++ MayoGenda/Controllers/DataControllerGenda.ts
@@ -1,7 +1,46 @@
+import express from "express";
+import requestDB from "../../serveur";
 
 const GendaController = {
-    getData: async () => {
-        
+    /**
+     * Cette fonction sert à récuperer des données pour MayoGenda
+     * @param req requete
+     * @param res reponse
+     * @param next next
+     * 
+     * @alpha Fonction en version alpha, car pas de db pour le moment
+     */
+    getData: async (req: express.Request, res: express.Response, next: express.NextFunction) => {
+        try {
+            const data = await requestDB(``);
+            res.status(200).send({ data });
+        } catch (err) {
+            res.status(500).send({ error: "An error occured.", messageError:err});
+        };
+    },
+    removeData: async (req: express.Request, res: express.Response, next: express.NextFunction) => {
+        try {
+            await requestDB(``);
+            res.status(200).send({ succes: "Data has been remove with success." });
+        } catch (err) {
+            res.status(500).send({ error: "An error occured", messageError:err})
+        }
+    },
+    patchData: async (req: express.Request, res: express.Response, next: express.NextFunction) => {
+        try {
+            await requestDB(``);
+            res.status(200).send({success: "Data has been modify with success"});
+        } catch (err) {
+            res.status(500).send({error: "An error occured.", messageError:err})
+        }
+    },
+    addData: async (req: express.Request, res: express.Response, next: express.NextFunction) => {
+        try {
+            await requestDB(``);
+            res.status(200).send({success:"Data has been add with success"})
+        } catch (err) {
+            res.status(500).send({error: "An error occured", messageError:err})
+        }
     }
 };
 
--- MayoGenda/Routes/DataRouteGenda.ts
+++ MayoGenda/Routes/DataRouteGenda.ts
@@ -4,6 +4,10 @@ const GendaRoute = express();
 //Import du controller de MayoGenda
 import GendaController from "../Controllers/DataControllerGenda";
 
-GendaRoute.get("/getdata", GendaController.getData)
+GendaRoute.get("/getdata", GendaController.getData);
+GendaRoute.delete("/removedata", GendaController.removeData);
+GendaRoute.patch("/patchdata", GendaController.patchData);
+GendaRoute.post("/adddata", GendaController.addData);
+
 
 export default GendaRoute;
